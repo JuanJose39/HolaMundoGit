b5fd6190daac0a51865f49dfc1aa56b125a1118d
josito <jsito3x@hotmail.com>
2022-04-07 13:25:10 +0200

    1 cambio Eclipse

--- HolaMundo/HolaMundo.java
+++ HolaMundo/HolaMundo.java
@@ -1,10 +1,11 @@
-package HolaMundo;
 
 public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("1 Cambio - Consola");
 		System.out.println("1 Cambio - Eclipse");
+
+
 
 	}
 
